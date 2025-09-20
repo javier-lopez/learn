d1f11b59b30e91c0077260fec1f1c46bb4f50067
Javier López <m@javier.io>
2025-09-20 09:41:12 -0600

    Update Chocolatey.ps1

--- powershell/Chocolatey.ps1
+++ powershell/Chocolatey.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install -y ag wget 7zip notepadplusplus
+choco install -y ag wget 7zip notepadplusplus gsudo microsoft-windows-terminal make mingw git localsend vcredist140
